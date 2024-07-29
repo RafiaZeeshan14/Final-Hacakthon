82b9b66e7e9a1903a30197ad646126219a53b2ae
Mahamilyas211 <mahamilyas211@gmail.com>
2024-07-30 00:17:35 +0500

    User FeePortal Added

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Signup } from './components/Registration/Signup'
 import { Login } from './components/Registration/Login'
 import Home from './components/DashBoard'
+import FeePortal from './components/UserFeePortal/FeePortal'
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
        <Route path='/' element={<Login />} />
        <Route path='/signup' element={<Signup />} />   
       <Route path='/dashboard' element={<Home />} />
+      <Route path='/feeportal' element={<FeePortal />} />
       </Routes>
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-text': 'linear-gradient(90deg, #88C343 0%, #285192 40%)',
+      },
+    },
   },
   plugins: [],
 }
