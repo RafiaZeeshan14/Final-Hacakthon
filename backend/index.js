f83511912100733bb088f4e1dc2401ec26bd96c9
RafiaZeeshan14 <fullstack786.dev01@gmail.com>
2024-08-10 18:25:25 +0500

    updated Links in backend

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 dbconnect()
 
 const corsOptions = {
-  origin: ['https://practice-fee-portal-jxzs.vercel.app', 'https://practice-fee-portal.vercel.app'], // List all allowed origins
+  origin: ['https://feeportal.vercel.app/', 'https://finalhacakthon.vercel.app/'], // List all allowed origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
