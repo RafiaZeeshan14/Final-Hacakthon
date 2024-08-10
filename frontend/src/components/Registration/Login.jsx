089a4c67a8d31b2b7594a2ffe10b41a332a41bf7
RafiaZeeshan14 <fullstack786.dev01@gmail.com>
2024-08-10 19:25:10 +0500

    images path updated

--- frontend/src/components/Registration/Login.jsx
+++ frontend/src/components/Registration/Login.jsx
@@ -33,19 +33,19 @@ export function Login() {
     <section className="relative min-h-screen">
     {/* Vector Image */}
     <img
-      src="/VectorBlueLeft.png"
+      src="/Images/VectorBlueLeft.png"
       alt="Vector Blue"
       className="absolute bottom-0 left-0 z-0"
     />
     <img
-      src="/VectorBlue.png"
+      src="/Images/VectorBlue.png"
       alt="Vector Blue"
       className="absolute top-0 right-0 z-0"
     />
     <div className="relative z-10 flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-14">
       <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
         <div className="mb-2 flex justify-center">
-          <img src="logo-stdnt.png" className="h-20" alt="Logo" />
+          <img src="/Images/logo-stdnt.png" className="h-20" alt="Logo" />
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight text-black">
           Sign in to your account
